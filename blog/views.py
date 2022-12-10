e4a185030fe8151290a1a1e7e07fbd5bd798a7da
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-12-10 05:25:49 +0000

    add base, home and about templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'August 27, 2018'
+    },
+
+    {
+        'author': 'bob',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'August 27, 2020',
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'about'})
